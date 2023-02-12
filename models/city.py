b13460f50d320153c710060f9cff0ef1426fcec7
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2023-02-11 17:04:48 -0800

    created state and city class

---
+++ models/city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" City Class Module for the AirBnB clone project """
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ The city class is a representation of a city 
+    
+    Attributes:
+        state_id (str): The state id.
+        name (str): The name of the city.
+    """
+    
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" State Class Module for the AirBnB clone project """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ State class 
+    
+    Attributes:
+        name (str): The name of the state
+    """
+    name = ""
